bd5fc45647f6f00b0cab34871c1852fcdf8dc3a1
U.Shamitha <shamitha19102002@gmail.com>
2023-04-19 18:09:52 +0530

    dsofsm

--- server/index.js
+++ server/index.js
@@ -61,8 +61,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-const YOUR_DOMAIN = 'https://stackoverflow-client-kgjp.onrender.com/Subscription';
+// const YOUR_DOMAIN = 'https://stackoverflow-client-kgjp.onrender.com/Subscription';
 // const YOUR_DOMAIN = 'http://localhost:3000/Subscription'
+const YOUR_DOMAIN = 'https://stackoverflow-sm-client.onrender.com/Subscription';
 
 app.post('/create-checkout-session', async (req, res) => {
   console.log(req.body.lookup_key)
